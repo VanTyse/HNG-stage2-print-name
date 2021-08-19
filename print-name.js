052e19bf6e723c62b91a8dfab14fddc751bd24b7
Kingsley Okafor <83710137+VanTyse@users.noreply.github.com>
2021-08-19 20:49:43 +0100

    Add files via upload

---
+++ print-name.js
@@ -0,0 +1,12 @@
+let name = prompt('Please enter your name', 'Okafor Kingsley Chukwunonso')
+let name_h1 = document.querySelector('.name');
+
+if (name===null || name===''){
+    let name1 = "You didn't input a name. \n However, my name is Kingsley Chukwunonso Okafor."
+    name_h1.innerHTML = name1;
+}
+else{
+    name_h1.innerHTML = name;
+}
+
+console.log(name);
