e0425e98e586a4394e828feed7170109a272cf67
Kingsley Okafor <83710137+VanTyse@users.noreply.github.com>
2021-08-19 22:32:41 +0100

    Update print-name.js

--- print-name.js
+++ print-name.js
@@ -2,11 +2,13 @@ let name = prompt('Please enter your name', 'Okafor Kingsley Chukwunonso')
 let name_h1 = document.querySelector('.name');
 
 if (name===null || name===''){
-    let name1 = "You didn't input a name. \n However, my name is Kingsley Chukwunonso Okafor."
+    let name1 = "You didn't input a name. However, my name is Kingsley Chukwunonso Okafor."
     name_h1.innerHTML = name1;
+    console.log(name1)
 }
 else{
     name_h1.innerHTML = name;
+    console.log(name)
 }
 
 console.log(name);
